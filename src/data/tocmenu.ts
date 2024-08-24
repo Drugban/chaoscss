f96481ad42eec7c3d7616719d2cc58043499cde2
drugban <drujbanjo@gmail.com>
2024-08-24 16:34:22 +0500

    fix: tocmenu.ts (#2)

--- src/data/tocmenu.ts
+++ src/data/tocmenu.ts
@@ -1,24 +1,25 @@
 export const tocmenu = [
-  {
-    label: "installation",
-    toc: [
-      {
-        href: "quick-start-with-cdn",
-        label: "start with cdn",
-      },
-      {
-        href: "start-with-package-manager",
-        label: "package menager",
-      },
-    ],
-  },
-  {
-    label: "download",
-    toc: [
-      {
-        href: "download-zip-compiled-css-files",
-        label: "download ZIP",
-      },
-    ],
-  },
-];
+	{
+		label: 'installation',
+		toc: [
+			{
+				href: 'quick-start-with-cdn',
+				label: 'start with cdn',
+				children: [{ href: '', label: '' }],
+			},
+			{
+				href: 'start-with-package-manager',
+				label: 'package menager',
+			},
+		],
+	},
+	{
+		label: 'download',
+		toc: [
+			{
+				href: 'download-zip-compiled-css-files',
+				label: 'download ZIP',
+			},
+		],
+	},
+]
